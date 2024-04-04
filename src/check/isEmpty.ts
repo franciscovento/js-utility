d5201539d40282d376a313a4cbc4b9481c07fb0d
venfran <fgvr92@gmail.com>
2024-04-04 14:41:03 -0700

    feat: adding new test

--- src/check/isEmpty.ts
+++ src/check/isEmpty.ts
@@ -1,7 +1,11 @@
+import { typeOf } from '@/misc'
+
 // Función que verifica si un array o un objeto está vacío y devuelve true o false si lo está o no.
-export function isEmpty (object: unknown): boolean {
+export function isEmpty(object: unknown): boolean {
+  if (typeOf(object) === 'null' || typeOf(object) === 'undefined') return true
   const safeObject = object
-  const isCountable = safeObject instanceof Object || safeObject instanceof Array
+  const isCountable =
+    safeObject instanceof Object || safeObject instanceof Array
   if (!isCountable) return true
   return Object.entries(safeObject).length === 0
 }
