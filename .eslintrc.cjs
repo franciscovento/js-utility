1cb538c0ebdb3988749119f31275c2f1c6ca0a18
venfran <fgvr92@gmail.com>
2024-04-05 14:22:32 -0700

    fix: configurations files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "ignorePatterns": [".eslintrc.*", "node_modules/", "dist/", "vite.config.ts", "commitlint.config.cjs"],
+    "ignorePatterns": [".eslintrc.cjs", "dist", "vite.config.ts", "commitlint.config.cjs", ".prettierrc", "**/__tests__/*"],
     "plugins": ["simple-import-sort"],
     "rules": {
         "simple-import-sort/imports": "error",
